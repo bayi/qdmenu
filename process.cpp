8e9406d40405088e2745c416a8ff7f173b45ccb1
Bayi <bayi@bayi.hu>
2020-10-26 19:06:19 +0100

    Strip field codes from Exec

--- process.cpp
+++ process.cpp
@@ -8,15 +8,17 @@ Process::Process(QObject *parent) : QObject(parent)
 void Process::start(QString program, bool isTerm)
 {
     // qDebug() << "Executing: " << program;
-    QStringList args = QProcess::splitCommand(program);
+    // Strip field codes from Exec= line
+    QString stripped = program.remove(QRegExp("%[a-zA-Z]")).trimmed();
+    QStringList args = QProcess::splitCommand(stripped);
     QString command = args.takeFirst();
     if (isTerm)
     {
         // qDebug() << "Starting terminal process: " << command << " args: " << args.join(',');
-        QProcess::startDetached("qdmenu-terminal", QStringList() << "-e" << command);
+        QProcess::startDetached("qdmenu-terminal", QStringList() << "-e" << command << args);
     } else {
         // qDebug() << "Starting process: " << command << " args: " << args.join(',');
-        QProcess::startDetached(command, QStringList());
+        QProcess::startDetached(command, args);
     }
 
     // Bye bye...
